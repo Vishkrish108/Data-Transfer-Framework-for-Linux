a6cdb93ceb1384c89be5784b7da30d4def28f94d
Varun Shenoy <varun123shenoy@gmail.com>
2024-11-16 13:21:06 +0530

    added server.py

---
+++ server.py
@@ -0,0 +1,21 @@
+import ip_utils
+from net_utils import Server
+import signal
+import threading
+
+def main():
+    ip_list, hostname = ip_utils.get_ip()
+    chosen_ip = ip_utils.choose_ip(ip_list)
+
+    server = Server()
+
+    def shutdown_handler(signum, frame):
+        server.shutdown_server()
+        exit(0)
+
+    signal.signal(signal.SIGINT, shutdown_handler)
+    server.start_server(chosen_ip, hostname)
+
+
+if __name__ == "__main__":
+    main()
