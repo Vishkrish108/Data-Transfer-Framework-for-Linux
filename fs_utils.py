c779773a167c3036353d4f357b3fdaf5de50dd4d
Vishal-Srinivasa <vishal27srinivasa@gmail.com>
2024-11-16 18:08:55 +0530

    minor bug fix

--- fs_utils.py
+++ fs_utils.py
@@ -11,30 +11,45 @@ class FS:
             os.makedirs(self.root_path)
         self.current_path = self.root_path
 
+    def is_valid_path(self, path):
+        path = os.path.normpath(os.path.join(self.current_path, path))
+        if os.path.abspath(path).startswith(os.path.abspath(self.root_path)):
+            return True
+        else:
+            return False
+
     def mkdir(self, directory_path):
         try:
-            os.makedirs(os.path.join(self.current_path, directory_path))
+            if not self.is_valid_path(directory_path):
+                return "Access denied"
+            os.makedirs(os.path.normpath(os.path.join(self.current_path, directory_path)))
             return "Directory created successfully"
         except OSError as error:
             return str(error)
 
     def rmdir(self, directory_path):
         try:
-            os.rmdir(os.path.join(self.current_path, directory_path))
+            if not self.is_valid_path(directory_path):
+                return "Access denied"
+            os.rmdir(os.path.normpath(os.path.join(self.current_path, directory_path)))
             return "Directory deleted successfully"
         except OSError as error:
             return str(error)
 
     def rm(self, file_path):
         try:
-            os.remove(os.path.join(self.current_path, file_path))
+            if not self.is_valid_path(file_path):
+                return "Access denied"
+            os.remove(os.path.normpath(os.path.join(self.current_path, file_path)))
             return "File deleted successfully"
         except OSError as error:
             return str(error)
 
     def cat(self, file_path):
         try:
-            with open(os.path.join(self.current_path, file_path), "rb") as file:
+            if not self.is_valid_path(file_path):
+                return "Access denied"
+            with open(os.path.normpath(os.path.join(self.current_path, file_path)), "rb") as file:
                 return file.read(1024).decode("utf-8")
         except OSError as error:
             return str(error)
@@ -43,24 +58,27 @@ class FS:
 
     def ls(self, directory_path="./"):
         try:
-            files = os.listdir(os.path.join(self.current_path, directory_path))
+            if not self.is_valid_path(directory_path):
+                return "Access denied"
+            files = os.listdir(os.path.normpath(os.path.join(self.current_path, directory_path)))
             return "\t".join(files)
         except OSError as error:
             return str(error)
 
     def cd(self, directory_path):
         try:
-            new_path = os.path.normpath(os.path.join(self.current_path, directory_path))
-            if not os.path.abspath(new_path).startswith(os.path.abspath(self.root_path)):
+            if not self.is_valid_path(directory_path):
                 return "Access denied"
-            self.current_path = new_path
+            self.current_path = os.path.normpath(os.path.join(self.current_path, directory_path))
             return "Directory changed successfully"
         except OSError as error:
             return str(error)
 
     def get(self, file_path):
         try:
-            with open(os.path.join(self.current_path, file_path), "rb") as file:
+            if not self.is_valid_path(file_path):
+                return "Access denied"
+            with open(os.path.normpath(os.path.join(self.current_path, file_path)), "rb") as file:
                 return file.read()
         except OSError as error:
             return str(error)
