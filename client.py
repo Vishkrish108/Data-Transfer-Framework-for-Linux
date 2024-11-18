a377970b668874ec5c707ae8df64342e85017b09
Vishal Srinivasa <vishal@VISHAL>
2024-11-18 20:51:37 +0530

    prettified client.py outputs

--- client.py
+++ client.py
@@ -1,5 +1,6 @@
 import ip_utils
 from net_utils import Client
+from colors import FG_GREEN, FG_RED, FG_CYAN, FG_YELLOW, FG_BG_CLEAR
 
 
 def main():
@@ -11,20 +12,20 @@ def main():
     client.run_scan(ip_range)
 
     if client.devices:
-        print("Available devices:")
+        print(f"{FG_GREEN}Available devices:{FG_BG_CLEAR}")
         for i, device in enumerate(client.devices):
             print(f"{i}: {device[0]} ({device[1]})")
 
-        choice = int(input("Select a device to connect to: "))
+        choice = int(input(f"{FG_CYAN}Select a device to connect to: {FG_BG_CLEAR}"))
         dest_ip = client.devices[choice][0]
         client.start_connection(dest_ip)
 
         while True:
-            command = input("Enter command (cat, ls, get, put, cd): ")
+            command = input(f"{FG_YELLOW}Enter command (cat, ls, get, put, cd): {FG_BG_CLEAR}")
             response = client.send_fs_command(command)
             print(response)
     else:
-        print("No devices found.")
+        print(f"{FG_RED}No devices found.{FG_BG_CLEAR}")
 
 
 if __name__ == "__main__":
