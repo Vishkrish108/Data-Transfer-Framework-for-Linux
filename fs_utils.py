74b981025389510dda029a83652efb2934a81648
Vishal Srinivasa <vishal@VISHAL>
2024-11-19 19:37:56 +0530

    fixed cd in fs_utils and prettified the file

--- fs_utils.py
+++ fs_utils.py
@@ -1,5 +1,5 @@
 import os
-
+from colors import FG_RED, FG_GREEN, FG_BG_CLEAR
 
 class FS:
 
@@ -21,64 +21,66 @@ class FS:
     def mkdir(self, directory_path):
         try:
             if not self.is_valid_path(directory_path):
-                return "Access denied"
+                return f"{FG_RED}Access denied{FG_BG_CLEAR}\n"
             os.makedirs(os.path.normpath(os.path.join(self.current_path, directory_path)))
-            return "Directory created successfully"
+            return f"{FG_GREEN}Directory created successfully{FG_BG_CLEAR}\n"
         except OSError as error:
-            return str(error)
+            return f"{FG_RED}{str(error)}{FG_BG_CLEAR}\n"
 
     def rmdir(self, directory_path):
         try:
             if not self.is_valid_path(directory_path):
-                return "Access denied"
+                return f"{FG_RED}Access denied{FG_BG_CLEAR}\n"
             os.rmdir(os.path.normpath(os.path.join(self.current_path, directory_path)))
-            return "Directory deleted successfully"
+            return f"{FG_GREEN}Directory deleted successfully{FG_BG_CLEAR}\n"
         except OSError as error:
-            return str(error)
+            return f"{FG_RED}{str(error)}{FG_BG_CLEAR}\n"
 
     def rm(self, file_path):
         try:
             if not self.is_valid_path(file_path):
-                return "Access denied"
+                return f"{FG_RED}Access denied{FG_BG_CLEAR}\n"
             os.remove(os.path.normpath(os.path.join(self.current_path, file_path)))
-            return "File deleted successfully"
+            return f"{FG_GREEN}File deleted successfully{FG_BG_CLEAR}\n"
         except OSError as error:
-            return str(error)
+            return f"{FG_RED}{str(error)}{FG_BG_CLEAR}\n"
 
     def cat(self, file_path):
         try:
             if not self.is_valid_path(file_path):
-                return "Access denied"
+                return f"{FG_RED}Access denied{FG_BG_CLEAR}\n"
             with open(os.path.normpath(os.path.join(self.current_path, file_path)), "rb") as file:
-                return file.read(1024).decode("utf-8")
+                return file.read(1024).decode("utf-8") + "\n"
         except OSError as error:
-            return str(error)
+            return f"{FG_RED}{str(error)}{FG_BG_CLEAR}\n"
         except UnicodeDecodeError as error:
-            return str(error)
+            return f"{FG_RED}{str(error)}{FG_BG_CLEAR}\n"
 
     def ls(self, directory_path="./"):
         try:
             if not self.is_valid_path(directory_path):
-                return "Access denied"
+                return f"{FG_RED}Access denied{FG_BG_CLEAR}\n"
             files = os.listdir(os.path.normpath(os.path.join(self.current_path, directory_path)))
-            return "\t".join(files)
+            return "\t".join(files) + "\n"
         except OSError as error:
-            return str(error)
+            return f"{FG_RED}{str(error)}{FG_BG_CLEAR}\n"
 
     def cd(self, directory_path):
         try:
             if not self.is_valid_path(directory_path):
-                return "Access denied"
+                return f"{FG_RED}Access denied{FG_BG_CLEAR}\n"
+            if not os.path.exists(os.path.normpath(os.path.join(self.current_path, directory_path))):
+                return f"{FG_RED}Directory does not exist{FG_BG_CLEAR}\n"
             self.current_path = os.path.normpath(os.path.join(self.current_path, directory_path))
-            return "Directory changed successfully"
+            return f"{FG_GREEN}Directory changed successfully{FG_BG_CLEAR}\n"
         except OSError as error:
-            return str(error)
+            return f"{FG_RED}{str(error)}{FG_BG_CLEAR}\n"
 
     def get(self, file_path):
         try:
             if not self.is_valid_path(file_path):
-                return "Access denied"
+                return f"{FG_RED}Access denied{FG_BG_CLEAR}\n"
             with open(os.path.normpath(os.path.join(self.current_path, file_path)), "rb") as file:
                 return file.read()
         except OSError as error:
-            return str(error)
+            return f"{FG_RED}{str(error)}{FG_BG_CLEAR}\n"
